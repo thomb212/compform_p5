35f6e01ac485e0f6c5f140468dffe950beb87d34
brinnathomsen <thomb212@gmail.com>
2016-02-24 00:39:51 -0500

    turtles - day 3

---
+++ week4/day3/README.md
@@ -0,0 +1,8 @@
+## Turtles: Day 1
+
+![Output](img/day2-02.png)
+![Output](img/day2-01.png)
+![Output](img/day2-03.png)
+![Output](img/day2-04.png)
+![Output](img/day2-06.png)
+![Output](img/day2-05.png)
---
+++ week4/day3/sketch.js
@@ -0,0 +1,91 @@
+// Week 4: Turtle Graphics
+// basic template sketch showing how to use the Turtle class
+var myTurtle;
+var color;
+var branchlength1;
+var branchlength2;
+var variant;
+
+function setup() {
+	createCanvas(640, 640);
+	noFill();
+	stroke(255);
+	background(250);
+	noLoop();
+
+
+	myTurtle = new Turtle();
+	color = random(0, 360);
+	branchlength1 = random(20, 50);
+	branchlength2 = random(20, 50);
+
+}
+
+
+
+function draw() {
+	// move to starting position (without drawing)
+	drawBranchPattern(0);
+	drawBranchPattern(50);
+
+}
+
+function drawBranchPattern(variant) {
+	myTurtle.penUp();
+	myTurtle.moveTo(320, 320);
+	myTurtle.turnTo(0);
+	myTurtle.penDown();
+
+	drawBranch(branchlength1 + variant);
+
+	myTurtle.penUp();
+	myTurtle.moveTo(320, 320);
+	myTurtle.turnTo(180);
+	myTurtle.penDown();
+
+	drawBranch(branchlength1 + variant);
+
+	myTurtle.penUp();
+	myTurtle.moveTo(320, 320);
+	myTurtle.turnTo(90);
+	myTurtle.penDown();
+
+	drawBranch(branchlength2 + variant);
+
+	myTurtle.penUp();
+	myTurtle.moveTo(320, 320);
+	myTurtle.turnTo(-90);
+	myTurtle.penDown();
+
+	drawBranch(branchlength2 + variant);
+
+}
+
+
+function drawBranch(length) {
+
+	if (length < 10) {
+		return;
+	}
+
+	colorMode(HSB);
+	stroke(color, 360, random(0, 100));
+	strokeWeight(length / 5);
+	// draw this branch
+	myTurtle.moveForward(length);
+
+	// left child
+	myTurtle.pushState();
+
+	myTurtle.turnLeft(20);
+
+	drawBranch(length * 0.85);
+	myTurtle.popState();
+
+	// right child
+	myTurtle.pushState();
+	myTurtle.turnRight(20);
+	drawBranch(length * 0.85);
+	myTurtle.popState();
+
+}
