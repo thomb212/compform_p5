962982884ec6d7da9c8c58cd1677bdd0693f21b5
brinnathomsen <thomb212@gmail.com>
2016-02-25 00:37:37 -0500

    day 4

--- week4/day4/sketch.js
+++ week4/day4/sketch.js
@@ -5,12 +5,19 @@ var color;
 var branchlength1;
 var branchlength2;
 var variant;
+var brightness;
+var curve;
+
+
+function preload() {
+	ellipse = loadImage("img/ellipse-01.png");
+}
 
 function setup() {
 	createCanvas(640, 640);
 	noFill();
 	stroke(255);
-	background(250);
+	background(0);
 	noLoop();
 
 
@@ -18,6 +25,8 @@ function setup() {
 	color = random(0, 360);
 	branchlength1 = random(20, 50);
 	branchlength2 = random(20, 50);
+	brightness = length * 5;
+	curve = random(30, 60);
 
 }
 
@@ -25,8 +34,12 @@ function setup() {
 
 function draw() {
 	// move to starting position (without drawing)
+
+	color = random(0, 360);
 	drawBranchPattern(20);
-	drawBranchPattern(50);
+	color = random(0, 360);
+	drawBranchPattern(10);
+	color = random(0, 360);
 	drawBranchPattern(0);
 }
 
@@ -64,27 +77,34 @@ function drawBranchPattern(variant) {
 
 function drawBranch(length) {
 
+
 	if (length < 10) {
 		return;
 	}
 
 	colorMode(HSB);
-	stroke(color, 360, length * 0.85);
-	strokeWeight(length / 5);
+	stroke(color, 360, 100, random(0, 255));
+	strokeWeight(length / 4);
+
+
+
 	// draw this branch
 	myTurtle.moveForward(length);
 
 	// left child
 	myTurtle.pushState();
-
-	myTurtle.turnLeft(20);
-
+	myTurtle.turnLeft(50);
 	drawBranch(length * 0.85);
 	myTurtle.popState();
+
+	myTurtle.image(ellipse, 5, 5);
 
 	// right child
 	myTurtle.pushState();
-	myTurtle.turnRight(20);
+	myTurtle.turnRight(50);
 	drawBranch(length * 0.85);
 	myTurtle.popState();
+
+
+
 }
