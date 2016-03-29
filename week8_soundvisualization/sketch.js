048e99b6bbb176b28f878197695597e04473955a
brinnathomsen <thomb212@gmail.com>
2016-03-28 23:20:18 -0400

    week8: day1

--- week8_soundvisualization/sketch.js
+++ week8_soundvisualization/sketch.js
@@ -8,52 +8,45 @@ var wave_scale = 1;
 
 
 function preload() {
-	sound = loadSound('assets/brooklyn-baby.mp3');
-	// fft = new p5.FFT(smoothing, collect_bins);
+	sound = loadSound('assets/let-her-go.mp3');
+	fft = new p5.FFT(smoothing, collect_bins);
 }
 
 function setup() {
 	createCanvas(512, 512);
 	//
-	// sound.loop();
+	sound.loop();
 }
 
 function draw() {
-	background(255);
-	ellipse(100, 100, 10, 10);
+	// background(255);
+	// ellipse(100, 100, 10, 10);
 
-	// // find the frequencies in the sound playing
-	// var spectrum = fft.analyze();
-	//
-	// fill(50);
+	// find the frequencies in the sound playing
+	var spectrum = fft.analyze();
+
+	fill(50);
 	// rect(0, 0, 512, 256);
-	// // show the first bins
-	// noStroke();
-	// fill(255);
-	// for (i = 0; i < show_bins; i++) {
-	// 	fill(spectrum[i]);
-	// 	rect(i * 512 / show_bins, 256 - spectrum[i] * wave_scale, 5, -5);
-	// }
-	//
-	// // find the peak value/bin among the shown bins
-	// var maxIndex = 0;
-	// var maxValue = spectrum[0];
-	// for (i = 0; i < show_bins; i++) {
-	// 	if (spectrum[i] > maxValue) {
-	// 		maxValue = spectrum[i];
-	// 		maxIndex = i;
-	// 	}
-	// }
-	//
-	// // draw a red square showing the peak
-	// fill(255, 0, 0);
-	// rect(maxIndex * 512 / show_bins, 256, 5, -5);
-	//
-	//
-	// // draw score
-	// for (i = 0; i < show_bins; i++) {
-	// 	fill(spectrum[i]);
-	// 	rect(frameCount / 2, 512 - i * 4, 5, 5);
-	//
-	// }
+	// show the first bins
+	noStroke();
+	fill(0);
+	for (i = 0; i < show_bins; i++) {
+		colorMode(HSB);
+		fill(spectrum[i] - 50, random(50, 150), spectrum[i] / 1.5, spectrum[i]);
+		ellipse(256 - spectrum[i] * wave_scale, i * 512 / show_bins, 50, -50);
+		ellipse(256 + spectrum[i] * wave_scale, i * 512 / show_bins, 50, -50);
+	}
+
+	// find the peak value/bin among the shown bins
+	var maxIndex = 0;
+	var maxValue = spectrum[0];
+	for (i = 0; i < show_bins; i++) {
+		if (spectrum[i] > maxValue) {
+			maxValue = spectrum[i];
+			maxIndex = i;
+		}
+	}
+
+
+
 }
